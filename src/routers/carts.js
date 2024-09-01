6439f897153b334c6fda6822a191f4cf3787138b
Jose Villa <josedvilla18@gmail.com>
2024-08-31 23:59:56 -0500

    unused file deleted

--- src/routers/carts.js
+++ src/routers/carts.js
@@ -7,13 +7,13 @@ import { createTicket, deleteTicket } from "../dao/controllers/ticketController.
 
 const router = Router();
 
-router.get('/:cid', auth(['admin', 'premium','user']), getCartById);
-router.post('/', auth(['admin', 'premium','user']), createCart);
-router.post('/:cid/product/:pid', auth(['admin', 'premium','user']), addProductInCart);
-router.delete('/:cid/products/:pid', auth(['admin', 'premium','user']), deleteProductsInCart);
-router.put('/:cid/products/:pid', auth(['admin', 'premium','user']), updateProductsInCart);
-router.delete('/:cid', auth(['admin', 'premium','user']), deleteCart);
-router.post('/:cid/purchase', auth(['premium','user', 'admin']), createTicket )
-router.delete('/delete/:tid',auth(['premium','user', 'admin']), deleteTicket);
+router.get('/:cid', auth(['admin', 'premium', 'user']), getCartById);
+router.post('/', auth(['admin', 'premium', 'user']), createCart);
+router.post('/:cid/product/:pid', auth(['admin', 'premium', 'user']), addProductInCart);
+router.delete('/:cid/products/:pid', auth(['admin', 'premium', 'user']), deleteProductsInCart);
+router.put('/:cid/products/:pid', auth(['admin', 'premium', 'user']), updateProductsInCart);
+router.delete('/:cid', auth(['admin', 'premium', 'user']), deleteCart);
+router.post('/:cid/purchase', auth(['premium', 'user', 'admin']), createTicket)
+router.delete('/delete/:tid', auth(['premium', 'user', 'admin']), deleteTicket);
 
 export default router;
